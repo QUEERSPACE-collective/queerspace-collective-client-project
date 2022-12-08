a1293901062ac28e9e60fbe7ad435aebdc39a0ff
Maia Johnson <maiaj1306@gmail.com>
2022-12-08 15:08:00 -0600

    clean up initial file to more accurately match project

--- database.sql
+++ database.sql
@@ -1,10 +1,46 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
+--Create Table "user"
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+	"id" SERIAL PRIMARY KEY,
+	"email" VARCHAR UNIQUE NOT NULL,
+	"password" VARCHAR,
+	"fname" VARCHAR,
+	"lname" VARCHAR,
+	"user_type" INT,
+	"pronouns" VARCHAR,
+	"profile_pic" VARCHAR,
+	"bio" VARCHAR,
+	"mentor_pair" VARCHAR
+);
+
+--CREATE TABLE "user_events"
+CREATE TABLE "user_events" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT REFERENCES "user",
+	"event_id" INT REFERENCES "events"
+);
+
+--CREATE TABLE "events"
+CREATE TABLE "events" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR,
+	"date_time" TIMESTAMP,
+	"location" VARCHAR,
+	"program_location" VARCHAR,
+	"type" INT,
+	"description" VARCHAR
+);
+
+--CREATE TABLE "questions"
+CREATE TABLE "questions" (
+	"id" SERIAL PRIMARY KEY,
+	"event_id" INT REFERENCES "events",
+	"question" VARCHAR
+	);
+
+--CREATE TABLE "answers"
+CREATE TABLE "answers" (
+	"id" SERIAL PRIMARY KEY,
+	"questions_id" INT REFERENCES "questions",
+	"user_id" INT REFERENCES "user",
+	"answer" VARCHAR
 );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,8 +6,10 @@ import './Footer.css';
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is, so it doesn't need 'connect()'
 
+// ADD Social Media links?
+
 function Footer() {
-  return <footer>&copy; Prime Digital Academy</footer>;
+  return <footer>&copy; QUEERSPACE collective</footer>;
 }
 
 export default Footer;
