85a07c62db77811d81f23d476c43a3c777ac5f67
Chris Maki <c.william.developer@gmail.com>
2022-12-09 09:41:14 -0600

    Fixed DB with dummy data

--- database.sql
+++ database.sql
@@ -10,7 +10,11 @@ CREATE TABLE "user" (
 	"bio" VARCHAR,
 	"mentorPair" VARCHAR
 	);
-
+DROP TABLE "answers";
+DROP TABLE "events";
+DROP TABLE "questions";
+DROP TABLE "user";
+DROP TABLE "userEvents";
 
 SELECT * FROM "user";
 SELECT * FROM "events";
@@ -55,14 +59,14 @@ JOIN "user"
 ON "user"."id" = "answers"."userId"
 ;
 --------
-INSERT INTO "user" ("email","password","fname","lname","userType","pronouns","profilePic","bio","mentorPair","hobbies")
-VALUES('chrismaki123@gmail.com', 'f', 'Chris', 'Maki', '3', 'he/him', 'url', 'am not chris', '1', 'rock climbing'),
-('janice4482@gmail.com', 'f', 'Janice', 'Janetson', '2', 'they/them', 'url', 'I like trains', '6', 'karate'),
-('billYahoo@yahoo.com', 'f', 'Bill', 'Billington', '3', 'they/them', 'url', 'kill bill is a movie', '2', 'watching films'),
-('carlosMarlos@hotmail', 'f', 'Carlos', 'Marlos', '3', 'he/him', 'url', 'my name is Carlos', '8', 'eating'),
-('gregarious@gmail.com', 'f', 'Greg', 'Gregson', '3', 'she/they', 'url', 'this my bio', '13', 'pickleball'),
-('scaryMary@aol.com', 'f', 'Mary', 'Merrison', '3', 'she/her', 'url', 'im a sweet lady', '12', 'sheep herding'),
-('jimmy@yahoo.com', 'f', 'Jimmy', 'Jimmerson', '3', 'he/she', 'url', 'jimbos the name', '11', 'crockpotting');
+INSERT INTO "user" ("email","password","fname","lname","userType","pronouns","profilePic","bio","mentorPair")
+VALUES('chrismaki123@gmail.com', 'f', 'Chris', 'Maki', '3', 'he/him', 'url', 'am not chris', '1'),
+('janice4482@gmail.com', 'f', 'Janice', 'Janetson', '2', 'they/them', 'url', 'I like trains', '6'),
+('billYahoo@yahoo.com', 'f', 'Bill', 'Billington', '3', 'they/them', 'url', 'kill bill is a movie', '2'),
+('carlosMarlos@hotmail', 'f', 'Carlos', 'Marlos', '3', 'he/him', 'url', 'my name is Carlos', '8'),
+('gregarious@gmail.com', 'f', 'Greg', 'Gregson', '3', 'she/they', 'url', 'this my bio', '13'),
+('scaryMary@aol.com', 'f', 'Mary', 'Merrison', '3', 'she/her', 'url', 'im a sweet lady', '12'),
+('jimmy@yahoo.com', 'f', 'Jimmy', 'Jimmerson', '3', 'he/she', 'url', 'jimbos the name', '11');
 CREATE TABLE "events" (
 	"id" SERIAL PRIMARY KEY,
 	"name" VARCHAR,
@@ -86,16 +90,21 @@ CREATE TABLE "userEvents" (
 	"userId" INT REFERENCES "user",
 	"eventId" INT REFERENCES "events"
 );
-
+INSERT INTO "userEvents" ("userId", "eventId")
+VALUES ('1', '2'), ('1', '3'), ('2', '1'), ('2', '2'), ('4', '1'), ('1','4');
 
 CREATE TABLE "questions" (
 	"id" SERIAL PRIMARY KEY,
 	"eventId" INT REFERENCES "events",
 	"question" VARCHAR
 );
+INSERT INTO "questions" ("eventId", "question")
+VALUES ('1', 'allergies?'), ('2', 'Desert preferences?'), ('3', 'dinner choices?'), ('4', 'do you need gloves?');
 CREATE TABLE "answers" (
 	"id" SERIAL PRIMARY KEY,
 	"questionId" INT REFERENCES "questions",
 	"userId" INT REFERENCES "user",
 	"answer" VARCHAR
 );
+INSERT INTO "answers" ("questionId", "userId", "answer")
+VALUES ('1', '1', 'user 1 answer'), ('2', '1', 'user 1 answer again'), ('3', '2','user 2 answer');
