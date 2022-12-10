bf9dc3d0877ea08e18acf8ca5528d7dbe3f683bd
Maia Johnson <maiaj1306@gmail.com>
2022-12-10 09:43:55 -0600

    create allusers reducer and dispatch command

--- src/components/AllUsersList/AllUsersList.jsx
+++ src/components/AllUsersList/AllUsersList.jsx
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import './AllUsersList.css';
 
 // CUSTOM COMPONENTS
 
+// On page load, GET all users
+
+
 function AllUsersList() {
-  
+  const dispatch = useDispatch();
+  const allUsersList = useSelector(store => store.allUsers);
+
+  useEffect(() => {
+    dispatch({ type: "FETCH_ALL_USERS" });
+  }, [])
+
 
   return (
-
+    <div>
     <h1>AllUsersList</h1>
+    </div>
     
   );
 }
