b1c1cfcf30bc448f1edf82b237f9f6c908211848
Chris Maki <c.william.developer@gmail.com>
2022-12-11 12:50:57 -0600

    Base nav bar functionality done. When no user logged in, will only show the logo and a login navigation button. Page refresh will also redirect the user. Logged in will show the hamburger drawer.

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import LoginPage from '../LoginPage/LoginPage';
 import './Drawer.css';
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 
 import {
   HashRouter as Router,
@@ -9,7 +11,6 @@ import {
   Switch,
   Link,
 } from 'react-router-dom';
-
 
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
@@ -29,85 +30,105 @@ import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import HomeRepairServiceIcon from '@mui/icons-material/HomeRepairService';
 import MoodIcon from '@mui/icons-material/Mood';
 import PersonSearchIcon from '@mui/icons-material/PersonSearch';
-// CUSTOM COMPONENTS
+import { useSelector } from 'react-redux';
 
 function Drawers() {
-   
-    const [state, setState] = React.useState({
-      left: false
-    });
+  const user = useSelector((store) => store.user); 
+  //Allows us to determine what the user sees,
+  // whether they are logged in or not
+
+  const [state, setState] = React.useState({
+    left: false
+  });
+
   const toggleDrawer = (anchor, open) => (event) => {
     if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
       return;
     }
-
     setState({ ...state, [anchor]: open });
   };
 
   const list = (anchor) => (
     <Router>
-    <Box 
-      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 } }
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
-    >
-      <List className='drawerText' sx={{ p: 0 }}> 
-        {[<Link to='./home'><p>Home</p></Link>, 
-          <Link to='./calendar'><p>Calendar</p></Link>, 
-            <Link to='./resources'><p>Resources</p></Link>, 
-              <Link to='./feedback'><p>Feedback Form</p> </Link>,
-                <Link to='./findmembers'><p>Find members</p></Link>].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton sx={{ p: 0 }}>
-              <ListItemIcon  >
-                {index == 0 ? <Link to='./home'><p className='drawerPadding'><CottageIcon /> </p></Link> : 
-                  index == 1 ? <Link to='./calendar'><p className='drawerPadding'><CalendarMonthIcon /></p></Link> :
-                  index == 2 ? <Link to='./resources'><p className='drawerPadding'><HomeRepairServiceIcon/></p> </Link> :
-                  index == 3 ? <Link to='./feedback'><p className='drawerPadding'><MoodIcon/> </p></Link> :
-                  index == 4 ? <Link to='./findmembers'><p className='drawerPadding'><PersonSearchIcon/></p>  </Link>: "" }
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['Logout'].map((text, index) => (
-          <Link to='./login'>
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <LogoutIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </LogoutIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-          </Link>
-        ))}
-      </List>
-    </Box>
-  </Router>
+      <Box
+        sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+        role="presentation"
+        onClick={toggleDrawer(anchor, false)}
+        onKeyDown={toggleDrawer(anchor, false)}
+      >
+        <List className='drawerText' sx={{ p: 0 }}>
+          {[<Link to='./home'><p>Home</p></Link>,
+          <Link to='./calendar'><p>Calendar</p></Link>,
+          <Link to='./resources'><p>Resources</p></Link>,
+          <Link to='./feedback'><p>Feedback Form</p> </Link>,
+          <Link to='./findmembers'><p>Find members</p></Link>].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton sx={{ p: 0 }} className='drawerPadding'>
+                <ListItemIcon  >
+                  {index == 0 ? <Link to='./home'><p><CottageIcon /> </p></Link> :
+                    index == 1 ? <Link to='./calendar'><p><CalendarMonthIcon /></p></Link> :
+                      index == 2 ? <Link to='./resources'><p><HomeRepairServiceIcon /></p></Link> :
+                        index == 3 ? <Link to='./feedback'><p><MoodIcon /> </p></Link> :
+                          index == 4 ? <Link to='./findmembers'><p><PersonSearchIcon /></p></Link> : ""}
+                </ListItemIcon>
+                <ListItemText primary={text}/>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['Logout'].map((text) => (
+            <Link to='./login'>
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <LogoutIcon>
+                    <InboxIcon/>
+                  </LogoutIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
+          ))}
+        </List>
+      </Box>
+    </Router>
   );
 
   return (
-<>
-    <div className='drawerContainer'>
-    {['left'].map((anchor) => (
-      <React.Fragment key={anchor}>
-        <Button onClick={toggleDrawer(anchor, true)}><MenuIcon/></Button>
-        <Drawer
-          anchor={anchor}
-          open={state[anchor]}
-          onClose={toggleDrawer(anchor, false)}
-        >
-          {list(anchor)}
-        </Drawer>
-      </React.Fragment>
-    ))}
-  </div>  
-</>  
+    <>
+      <div>
+        {!user.id && (
+          <div>
+            <Redirect to='/home'></Redirect> 
+            {/* Set to 'home' right now, could also be 'login' page */}
+            {/* Whenever the page is reloaded, it will take us back to /home */}
+            
+            <Link className="navLink navLogin" to="/login"  >
+              Login
+            </Link> 
+
+          </div>
+        )}
+      </div>
+
+      <div className='drawerContainer'>
+        {user.id && (
+          ['left'].map((anchor) => (
+            <React.Fragment key={anchor}>
+              <Button onClick={toggleDrawer(anchor, true)}><MenuIcon /></Button>
+              <Drawer
+                anchor={anchor}
+                open={state[anchor]}
+                onClose={toggleDrawer(anchor, false)}
+              >
+                {list(anchor)}
+              </Drawer>
+            </React.Fragment>
+          ))
+        )}
+      </div>
+    </>
   );
 }
 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,56 +1,54 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 import { useSelector } from 'react-redux';
 import QSClogo from '../QSClogo/QSClogo';
 import Drawers from '../Drawer/Drawer';
+import LoginPage from '../LoginPage/LoginPage';
+
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+  Link,
+} from 'react-router-dom';
+
 function Nav() {
   const user = useSelector((store) => store.user);
 
   return (
     <>
+     {/* The drawers component dictates whether the user will see the
+         hamburger menu or not */}   
     <div className="nav">
-    <span className='drawersContainer'>
-    <Drawers />
-    </span>
+      <span className='drawersContainer'>
+        <Drawers />
+      </span>
       <header className='myHeader'>
-           
-
-          
-          <Link to="/home">
-            <QSClogo />
-          </Link>
-        </header>
-        </div>
-      <div>
-
-        {/* If no user is logged in, show these links */}
-        {!user.id && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login
-          </Link>
-        )}
-
-        {/* If a user is logged in, show these links */}
-        {user.id && (
-          <>
-            <Link className="navLink" to="/user">
-              Home
-            </Link>
-
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
-
-            <LogOutButton className="navLink" />
-          </>
-        )}
-
-        <Link className="navLink" to="/AboutPage">
-          About
+        <Link to="/home">
+          <QSClogo />
         </Link>
+        
+      </header>
+      
+    </div>
+
+    <div>
+      {user.id && (
+        <>
+          <Link className="navLink" to="/user">
+            Home
+          </Link>
+
+          <Link className="navLink" to="/info">
+            Info Page
+          </Link>
+
+          <LogOutButton className="navLink" />
+        </>
+      )}
+
       
     </div>
     </>
