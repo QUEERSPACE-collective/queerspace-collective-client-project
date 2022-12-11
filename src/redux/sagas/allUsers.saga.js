08775e414b4f042385775bb32ca5db3e4747de6e
Maia Johnson <maiaj1306@gmail.com>
2022-12-11 17:40:13 -0600

    create edit user saga and reducer

--- src/redux/sagas/allUsers.saga.js
+++ src/redux/sagas/allUsers.saga.js
@@ -15,8 +15,29 @@ function* fetchAllUsers() {
     }
 }
 
+function* fetchEditUser(action) {
+    try {
+        const response = yield axios.get(`/api/allusers/${action.payload}`);
+
+        yield put({
+            type: "SET_EDIT_USER",
+            payload: response.data
+        });
+    } catch (err) {
+        console.error('get editing user error', err);
+    }
+}
+
+function* saveUser(action) {
+    if (action.payload.id) {
+        yield axios.put(`/api/allusers/${action.payload.id}`, action.payload);
+    }
+}
+
 function* allUsersSaga() {
     yield takeLatest('FETCH_ALL_USERS', fetchAllUsers);
+    yield takeLatest('FETCH_EDIT_USER', fetchEditUser);
+    yield takeLatest('SAVE_USER', saveUser);
 }
 
 export default allUsersSaga;
