c5f55ffeb9defc126e482e8b7d51fd3234ee7ba7
Rider <goejrider@gmail.com>
2022-12-13 10:13:56 -0600

    built html for resource form

--- database.sql
+++ database.sql
@@ -17,7 +17,8 @@ CREATE TABLE "programLocations" (
 );
 
 INSERT INTO "programLocations" ("locationName")
-VALUES ('St. Cloud'), ('Twin Cities');
+VALUES  ('Twin Cities'), ('St. Cloud');
+
 
 CREATE TABLE "events" (
 	"id" SERIAL PRIMARY KEY,
@@ -25,15 +26,26 @@ CREATE TABLE "events" (
 	"dateTime" TIMESTAMP,
 	"location" VARCHAR,
 	"programLocationID" INT REFERENCES "programLocations",
-	"type" INT,
+	"type" INT REFERENCES "eventTypes",
+	"attendeeMax" INT,
+	"hasVolunteers" BOOLEAN,
+	"volunteerMax" INT,
 	"description" VARCHAR
 );
 
-CREATE TABLE "userEvents" (
+CREATE TABLE "eventTypes" (
 	"id" SERIAL PRIMARY KEY,
-	"userId" INT REFERENCES "user" ON DELETE CASCADE,
-	"eventId" INT REFERENCES "events" ON DELETE CASCADE
+	"eventType" VARCHAR NOT NULL
 );
+
+INSERT INTO "eventTypes" (
+	"eventType"
+)
+VALUES 
+('Group Hangout'),
+('Family Event'),
+('Training Event'),
+('Mentor Only');
 
 CREATE TABLE "questions"(
 	"id" SERIAL PRIMARY KEY,
@@ -48,32 +60,8 @@ CREATE TABLE "answers" (
 	"answer" VARCHAR
 );
 
-INSERT INTO "user" ("username","password","fname","lname","userType","pronouns","profilePic","bio","mentorPair")
-VALUES('chrismaki123@gmail.com', 'f', 'Chris', 'Maki', '3', 'he/him', 'url', 'am not chris', '1'),
-('janice4482@gmail.com', 'f', 'Janice', 'Janetson', '2', 'they/them', 'url', 'I like trains', '6'),
-('billYahoo@yahoo.com', 'f', 'Bill', 'Billington', '3', 'they/them', 'url', 'kill bill is a movie', '2'),
-('carlosMarlos@hotmail', 'f', 'Carlos', 'Marlos', '3', 'he/him', 'url', 'my name is Carlos', '8'),
-('gregarious@gmail.com', 'f', 'Greg', 'Gregson', '3', 'she/they', 'url', 'this my bio', '13'),
-('scaryMary@aol.com', 'f', 'Mary', 'Merrison', '3', 'she/her', 'url', 'im a sweet lady', '12'),
-('jimmy@yahoo.com', 'f', 'Jimmy', 'Jimmerson', '3', 'he/she', 'url', 'jimbos the name', '11');
-
-
-INSERT INTO "events" ("name", "dateTime", "location", "programLocationID", "type", "description")
-VALUES ('BBQ', '04/22/2022', 'Prime Digital Academy', '1', '2', 'we out here'),
-('Monthly get-together', '04/22/2022','Prime Digital Academy', '1', '2', 'come hang out!'),
-('Picnic', '04/22/2022', 'alleyway somehwere', '2','1','no plus ones'),
-('Snowboarding', '04/22/2022', 'Wild Mountain',  '1','5', 'dress warm!');
-		
-		
-INSERT INTO "userEvents" ("userId", "eventId")
-VALUES ('1', '2'), ('1', '3'), ('2', '1'), ('2', '2'), ('4', '1'), ('1','4');
-
-
-INSERT INTO "questions" ("eventId", "question")
-VALUES ('1', 'allergies?'), ('2', 'Desert preferences?'), ('3', 'dinner choices?'), ('4', 'do you need gloves?');
-
-INSERT INTO "answers" ("questionId", "userId", "answer")
-VALUES ('1', '1', 'user 1 answer'), ('2', '1', 'user 1 answer again'), ('3', '2','user 2 answer');
-
-
-
+CREATE TABLE "userEvents" (
+	"id" SERIAL PRIMARY KEY,
+	"userId" INT REFERENCES "user",
+	"eventId" INT REFERENCES "events"
+);
--- src/components/AddResourceForm/AddResourceForm.jsx
+++ src/components/AddResourceForm/AddResourceForm.jsx
@@ -1,16 +1,34 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import './AddResourceForm.css';
 
 // CUSTOM COMPONENTS
 
 function AddResourceForm() {
-  
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   return (
+    <>
+      <h1>Add Resource Form</h1>
 
-    <h1>AddResourceForm</h1>
-    
+      <form>
+        <label for='newResourceName'>Resource Name: </label>
+        <input type='text' id='newResourceName'></input>
+        <br/>
+        <br/>
+        <label for='newResourceDescription'>Description: </label>
+        <input type='text' id='newResourceDescription'></input>
+        <br/>
+        <br/>
+        <label for='newResourceLink'>Link: </label>
+        <input type='url' id='newResourceLink'></input>
+        <br/>
+        <br/>
+        <button type='submit'>Add Resource</button>
+      </form>
+    </>
   );
 }
 
