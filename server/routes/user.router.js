4990ccb795410160ebe9454aac2224cb48979758
Maia Johnson <maiaj1306@gmail.com>
2022-12-13 17:27:55 -0600

    create post request in user router

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -5,7 +5,9 @@ const {
 const encryptLib = require('../modules/encryption');
 const pool = require('../modules/pool');
 const userStrategy = require('../strategies/user.strategy');
-
+"use strict";
+const nodemailer = require("nodemailer");
+require('dotenv').config();
 const router = express.Router();
 
 // Handles Ajax request for user information if user is authenticated
@@ -90,6 +92,43 @@ router.delete('/events/:id', rejectUnauthenticated, (req, res) => {
 
 // Send email to user to register
 router.post('/adduser', (req, res) => {
-  
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+//   let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.office365.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.EMAIL_USERNAME, // generated ethereal user
+      pass: process.env.EMAIL_PASSWORD, // generated ethereal password
+    },
+    tls: {
+        ciphers: 'SSLv3'
+    }
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: 'joanofchoir@outlook.com', // sender address
+    to: "maiaj1306@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+main().catch(console.error);
 })
 module.exports = router;
