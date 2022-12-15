030dbc3e54256d26d9334a181b0e0b33bdb8d9a4
Maia Johnson <maiaj1306@gmail.com>
2022-12-15 14:20:20 -0600

    move registration form to adduserform page

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -16,23 +16,78 @@ router.get('/', rejectUnauthenticated, (req, res) => {
   res.send(req.user);
 });
 
+// Password generator function
+function generatePW() {
+  var pass = '';
+  var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 
+          'abcdefghijklmnopqrstuvwxyz0123456789@#$';
+    
+  for (let i = 1; i <= 8; i++) {
+      var char = Math.floor(Math.random()
+                  * str.length + 1);
+        
+      pass += str.charAt(char)
+  }
+    
+  return pass;
+};
+
 // Handles POST request with new user data
 // The only thing different from this and every other post we've seen
 // is that the password gets encrypted before being inserted
 router.post('/register', (req, res, next) => {
   const username = req.body.username;
-  const password = encryptLib.encryptPassword(req.body.password);
-  const firstName = req.body.fname;
-  const lastName = req.body.lname;
+  const pw = generatePW();
+  const password = encryptLib.encryptPassword(pw);
   const userType = req.body.userType;
-  const pronouns = req.body.pronouns;
-  const profilePic = req.body.profilePic;
-  const bio = req.body.bio;
+  
+  // async..await is not allowed in global scope, must use a wrapper
+  async function main() {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+  //   let testAccount = await nodemailer.createTestAccount();
+  
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp.office365.com",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: process.env.EMAIL_USERNAME, // generated ethereal user
+        pass: process.env.EMAIL_PASSWORD, // generated ethereal password
+      },
+      tls: {
+          ciphers: 'SSLv3'
+      }
+    });
+  
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: process.env.EMAIL_USERNAME, // sender address TODO: SWITCH TO QSC's email (cannot be gmail!!)
+      to: username, // list of receivers
+      subject: "Thank you for signing up!", // Subject line
+      text: `When you sign in, here are your credentials:
+              username: ${username}
+              password: ${pw}
+              Please follow the link to sign up:
+              http://localhost:3000/login`, // plain text body
+      // html: "<b>Hello world?</b>", // html body
+    });
+  
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  }
+  
+  main().catch(console.error);
 
-  const queryText = `INSERT INTO "user" (username, password, fname, lname, "userType", pronouns, "profilePic", bio)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id`;
+  const queryText = `INSERT INTO "user" (username, password, "userType")
+    VALUES ($1, $2, $3) RETURNING id`;
   pool
-    .query(queryText, [username, password, firstName, lastName, userType, pronouns, profilePic, bio])
+    .query(queryText, [username, password, userType])
     .then(() => res.sendStatus(201))
     .catch((err) => {
       console.log('User registration failed: ', err);
@@ -93,69 +148,6 @@ router.delete('/events/:id', rejectUnauthenticated, (req, res) => {
     console.log('error deleting user event in router', error)
     res.sendStatus(500)
   })
-})
-
-// Password generator function
-function generatePW() {
-  var pass = '';
-  var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 
-          'abcdefghijklmnopqrstuvwxyz0123456789@#$';
-    
-  for (let i = 1; i <= 8; i++) {
-      var char = Math.floor(Math.random()
-                  * str.length + 1);
-        
-      pass += str.charAt(char)
-  }
-    
-  return pass;
-}
-
-// Send email to user to register
-router.post('/adduser', (req, res) => {
-console.log('this is req.body: ', req.body);
-const userEmail = req.body.data.username;
-const pw = generatePW();
-// async..await is not allowed in global scope, must use a wrapper
-async function main() {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-//   let testAccount = await nodemailer.createTestAccount();
-
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "smtp.office365.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: process.env.EMAIL_USERNAME, // generated ethereal user
-      pass: process.env.EMAIL_PASSWORD, // generated ethereal password
-    },
-    tls: {
-        ciphers: 'SSLv3'
-    }
-  });
-
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: 'joanofchoir@outlook.com', // sender address TODO: SWITCH TO QSC's email (cannot be gmail!!)
-    to: userEmail, // list of receivers
-    subject: "Thank you for signing up!", // Subject line
-    text: `When you sign in, your username will be your email. 
-            Please follow the link to sign up:
-            http://localhost:3000/registration/#/registration/${pw}`, // plain text body
-    // html: "<b>Hello world?</b>", // html body
-  });
-
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-}
-
-main().catch(console.error);
 })
 
 // GET specific user
