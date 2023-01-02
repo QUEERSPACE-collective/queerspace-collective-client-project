fdcdf8c3193f767dc0835ce9070b77901a9d3fd7
Chris Maki <c.william.developer@gmail.com>
2023-01-02 13:48:11 -0600

    Database working on one keystroke

--- database.sql
+++ database.sql
@@ -11,6 +11,15 @@ CREATE TABLE "user" (
 	"mentorPair" VARCHAR
 );
 
+INSERT INTO "user" ("username","password","fname","lname","userType","pronouns","profilePic","bio","mentorPair")
+VALUES('chrismaki123@gmail.com', 'f', 'Chris', 'Maki', '3', 'he/him', 'url', 'am not chris', '1'),
+('janice4482@gmail.com', 'f', 'Janice', 'Janetson', '2', 'they/them', 'url', 'I like trains', '6'),
+('billYahoo@yahoo.com', 'f', 'Bill', 'Billington', '3', 'they/them', 'url', 'kill bill is a movie', '2'),
+('carlosMarlos@hotmail', 'f', 'Carlos', 'Marlos', '3', 'he/him', 'url', 'my name is Carlos', '8'),
+('gregarious@gmail.com', 'f', 'Greg', 'Gregson', '3', 'she/they', 'url', 'this my bio', '13'),
+('scaryMary@aol.com', 'f', 'Mary', 'Merrison', '3', 'she/her', 'url', 'im a sweet lady', '12'),
+('jimmy@yahoo.com', 'f', 'Jimmy', 'Jimmerson', '3', 'he/she', 'url', 'jimbos the name', '11');
+
 CREATE TABLE "programLocations" (
 	"id" SERIAL PRIMARY KEY,
 	"locationName" VARCHAR
