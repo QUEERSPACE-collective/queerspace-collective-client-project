380e8e3ae5a63b8a7d122e92d82c683a307dd544
Astrid Pulse <astridcpulse@gmail.com>
2023-01-03 12:55:06 -0600

    datetimened reducer and endpoint and form element created

--- server/routes/event.router.js
+++ server/routes/event.router.js
@@ -132,11 +132,11 @@ router.delete('/:id', rejectUnauthenticated, async (req, res) => {
 
 router.post('/', (req, res) => {
     console.log('reqbody is', req.body);
-    let sqlText = 
-    `INSERT INTO "events" ("name","dateTime","location","programLocationID","type","attendeeMax","hasVolunteers", "volunteerMax", "description")
-    VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9)
+    let sqlText = `INSERT INTO "events" ("name","dateTime", "dateTimeEnd", "location","programLocationID","type","attendeeMax","hasVolunteers", "volunteerMax", "description")
+    VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)
     RETURNING "id";`;
-    let sqlParams = [req.body.data.name, req.body.data.dateTime, req.body.data.location, req.body.data.programLocationID, req.body.data.type, req.body.data.attendeeMax, req.body.data.hasVolunteers, req.body.data.volunteerMax, req.body.data.description];
+    
+    let sqlParams = [req.body.data.name, req.body.data.dateTime, req.body.data.dateTimeEnd, req.body.data.location, req.body.data.programLocationID, req.body.data.type, req.body.data.attendeeMax, req.body.data.hasVolunteers, req.body.data.volunteerMax, req.body.data.description];
     pool.query(sqlText, sqlParams)
         .then(dbRes=>{
             const eventId = dbRes.rows[0].id;
