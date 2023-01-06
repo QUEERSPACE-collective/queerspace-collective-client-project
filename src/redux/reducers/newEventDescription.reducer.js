65c8eba4388f3520764fd489911e1645f76a4844
Rider <goejrider@gmail.com>
2023-01-06 09:44:12 -0600

    finish autofill

--- src/redux/reducers/newEventDescription.reducer.js
+++ src/redux/reducers/newEventDescription.reducer.js
@@ -2,6 +2,9 @@ const newEventDescription = (state='', action)=>{
     switch(action.type){
         case 'SET_NEW_EVENT_DESCRIPTION':
             return action.payload;
+        case 'EVENT_FORM_FILLER':
+            return 'Since this went so well last time, we\’ve decided to have another ice skating get together on Lake Harriet!'
+
         case 'CLEAR_NEW_EVENT_FORM':
             return '';
     }
