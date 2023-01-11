a28116a7bc8265a11b9c8ae778da3f35d6d4834f
Astrid Pulse <astridcpulse@gmail.com>
2023-01-11 15:31:40 -0600

    comment on celendareventlist component

--- src/components/CalendarEventList/CalendarEventList.jsx
+++ src/components/CalendarEventList/CalendarEventList.jsx
@@ -5,20 +5,24 @@ import { useEffect } from 'react';
 import Button from '@mui/material/Button';
 import './CalendarEventList.css';
 import 'add-to-calendar-button/assets/css/atcb.css';
+//using moment to format dates
 import moment from 'moment-timezone';
+
 
 function CalendarEventList() {
   const eventList = useSelector(store => store.event);
   const user = useSelector(store => store.user);
 
+
   return (
     <>
       <ul>
-
+        {/* display all upcoming events for users who are not volunteers */}
         {eventList && eventList.map(event => (
           (user.userType > 1 && (
             <li key={event.id}>
               {event.name}<br />
+
               Date: {moment(event.dateTime).format("dddd, MMMM Do YYYY, h:mm:ss A")}<br />
               Location: {event.location}<br />
               <Link to={`/EventDetails/event/${event.id}`}>
@@ -42,6 +46,7 @@ function CalendarEventList() {
         ))}
       </ul>
       <ul>
+        {/* if user is a volunteer, and the event needs volunteers, then display all those events */}
         {eventList && eventList.map(event => (
           ((user.userType == 1 && event.hasVolunteers) && (
             <li key={event.id}>
