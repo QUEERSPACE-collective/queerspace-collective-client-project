3149ada296371db8ba614121b0fbbe8fb1cc8260
Astrid Pulse <astridcpulse@gmail.com>
2023-01-11 17:01:45 -0600

    comment on EditProfilePicture componenet

--- src/components/EditProfilePicture/EditProfilePicture.jsx
+++ src/components/EditProfilePicture/EditProfilePicture.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Upload from '../ImageUpload/ImageUpload';
+import ImageUpload from '../ImageUpload/ImageUpload';
 import { useEffect, useState } from "react";
 import Button from '@mui/material/Button';
 import { useHistory } from 'react-router-dom';
 import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
 
 function EditProfilePicture() {
+
   const history = useHistory();
   useEffect(() => {
     pageFadeIn()
@@ -18,8 +19,10 @@ function EditProfilePicture() {
     setTimeout(() => document.body.classList.remove("noOpacity"), 100);
     setTimeout(() => document.body.classList.add("withOpacity"), 100);
   };
+
   return (
     <div>
+      {/* back button */}
         <Button 
           onClick = {() => history.push('/homepage')}
           sx = {{fontWeight: 'bold', wordSpacing: 1, color: '#357590',                
@@ -30,7 +33,7 @@ function EditProfilePicture() {
               <ArrowCircleLeftIcon/> Home
         </Button>
       <h1>Edit Profile Picture</h1>
-      <Upload />
+      <ImageUpload />
     </div>
   );
 };
